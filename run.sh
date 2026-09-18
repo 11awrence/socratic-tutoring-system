3f96123495122a0952a3624b549bc46350823a80
青山蓮奈 <lenaaoyama@Chaos-Gremlins-MacBook-Pro.local>
2026-09-18 10:06:58 +0200

    add conda env

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 set -e
-source .venv/bin/activate
+# source .venv/bin/activate
+source "$(conda info --base)/etc/profile.d/conda.sh"
+conda activate tutor_env
 uvicorn api:app --port 8000 &
 chainlit run chainlit.py --port 8001
